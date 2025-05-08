65b23660d6de69fed9e405bd4e18a6239512267d
EstherMutua <esthermutua16@gmail.com>
2025-05-08 16:42:23 +0300

    Hate tests

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,13 @@
 
 class Dog:
     # Class body goes here
+    def __init__(self, name="Unknown" ):
+        self.name = name
+        
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")  
+
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
+    def __init__(self, name="Person", age=0):
+        self.name = name
+        self.age = age
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
